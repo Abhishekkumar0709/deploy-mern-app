aa480e90476214455a80046e73ba1447b136d2a0
Abhishekkumar0709 <abhisuarez9@gmail.com>
2024-09-04 13:50:20 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -8,10 +8,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/auth' , authrouter);
 app.use('/products',ProductRouter);
+app.use(
+  cors({
+    origin: 'https://deploy-mern-app-r0nsllyou-abhisheks-projects-26d9c136.vercel.app', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+    credentials: true, // Allow credentials such as cookies
+  })
+);
 
 const Port = "4040";
 app.listen(Port, ()=>{
